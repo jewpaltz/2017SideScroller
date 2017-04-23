3005076c979162780d4c442453032f0ce73a4c59
Moshe Plotkin <mplotkin@jewpaltz.com>
2017-04-23 09:42:20 -0400

    Add blast radius variable and comments

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour {
 
+    public float blastRadius = 5;
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         var player = coll.gameObject.GetComponent<Player>();
         if (player != null) {
+
+            //  Get a reference to all enemies
             var enemies = FindObjectsOfType<Enemy>();
+
+            //  Loop through each enemy in the list
             foreach(var e in enemies) {
-                if(Vector3.Distance(this.transform.position, e.transform.position) < 10) {
+
+                //  Check if that enemy is within the blast radius
+                if(Vector3.Distance(this.transform.position, e.transform.position) < blastRadius) {
+
+                    //  Set that enemy to NOT-Active
                     e.gameObject.SetActive(false);
                 }
             }
+
+            //  Set myself (aka the bomb) to NOT-Active. That way the bomb disappears, and can not be picked up again.
+            gameObject.SetActive(false);
         }
     }
 }
